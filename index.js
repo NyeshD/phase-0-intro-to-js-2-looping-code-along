f30abb03d656a9a3b81fd423584b5ecc5b8d20fa
Nyesh Dotson <dotsonnyesh@gmail.com>
2023-03-24 15:22:03 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = ['Guadalupe','Ollie','Aki']
+function writeCards(names,value) {
+    let messages = []
+    for (let i=0; i < names.length; i++) {
+messages.push(`Thank you, ${names[i]}, for the wonderful ${value} gift!`)
+}
+return messages 
+}
+ writeCards(["Guadalupe","Ollie","Aki"], "surprise")
+
+ function countDown() {
+    let number = 10
+    while (number >= 0) {
+        console.log(--number)
+    }
+ }
